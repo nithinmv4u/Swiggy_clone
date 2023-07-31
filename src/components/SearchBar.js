0caa145582a968f8ba39a966e5d1d39ecf1818a1
nithinmv4u <nithinmv4u@gmail.com>
2023-07-31 23:05:35 +0530

    usestate to input value

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const SearchBar = () => {
+    // let searchTxt = "KFC"
+    const [searchTxt, setSearchTxt] = useState("Indian Cuisines")
     return (
         <div className='search'>
-            <input className='search_img' type="text" placeholder='Search' />
+            <input 
+            className='search_img' 
+            type="text" 
+            placeholder='Search' 
+            value={searchTxt}
+            onChange={(e) => {
+                setSearchTxt(e.target.value)
+            }}
+            />
             <span className="material-symbols-outlined">search</span>
         </div>
 
