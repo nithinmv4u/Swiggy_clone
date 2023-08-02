337d71e1fff8b36415e677883466861904f2d541
nithinmv4u <nithinmv4u@gmail.com>
2023-08-02 19:06:56 +0530

    search & filter with useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,51 @@
 import RestaurantList from "./RestaurantList";
-import { restaurantList } from "../constants"
+import { restaurantList } from "../constants";
+import { useState } from "react";
 
-const Body = () => {
+function filterRestaurant(restaurants, searchTxt){
+    console.log(restaurants);
+    console.log(searchTxt);
+    const data = restaurants.filter((restaurant) =>
+        restaurant.data.name.includes(searchTxt)
+    );
+    console.log(data);
+    return data
+}
+
+const Body = () => { 
+
+    const [searchTxt, setSearchTxt] = useState("");
+    const [restaurants, setRestaurant] = useState(restaurantList);
+    // console.log(restaurants);
+
     return (
-        <div className='restaurant-list'>
-            {
-                restaurantList.map((restaurant) => {
-                    return (<RestaurantList {...restaurant.data} key={restaurant.data.id}/>)
-                })
-            }
-        </div>
+        <>
+            <div className='search'>
+                <input 
+                className='search_img' 
+                type="text" 
+                placeholder='Search' 
+                value={searchTxt}
+                onChange={(e) => {
+                    setSearchTxt(e.target.value)
+                    // setRestaurant(restaurantList)
+                }}
+                />
+                <a href="#" onClick={() =>{
+                    const data = filterRestaurant(restaurantList, searchTxt);
+                    console.log(data);
+                    setRestaurant(data);
+                }} ><span className="material-symbols-outlined">search</span> </a> 
+            </div>
+            <div className='restaurant-list'>
+                {
+                    restaurants.map((restaurant) => {
+                        return (<RestaurantList {...restaurant.data} key={restaurant.data.id}/>)
+                    })
+                }
+            </div>
+        </>
+
 
     )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,43 +1,38 @@
 import { useState } from "react";
+import { restaurantList } from "../constants";
 
 const SearchBar = () => {
-    // let searchTxt = "KFC"
-    const [searchTxt, setSearchTxt] = useState("Indian Cuisines")
+//     // let searchTxt = "KFC"
+//     const [searchTxt, setSearchTxt] = useState("Indian Cuisines")
 
-    const [searchClick, setSearchClick] = useState(false)
+//     const [restaurants, setRestaurants] = useState(restaurantList)
 
-    return (
-        <>
-            <div className='search'>
-                <input 
-                className='search_img' 
-                type="text" 
-                placeholder='Search' 
-                value={searchTxt}
-                onChange={(e) => {
-                    setSearchTxt(e.target.value)
-                }}
-                />
-                <a href="#" onClick={() =>{
-                    console.log("clicked " + searchClick);
-                    if (searchClick === false){
-                        setSearchClick(true);
-                    }  
-                    else {
-                        setSearchClick(false);
-                    }
-                }} ><span className="material-symbols-outlined">search</span> </a>
+//     return (
+//         <>
+//             <div className='search'>
+//                 <input 
+//                 className='search_img' 
+//                 type="text" 
+//                 placeholder='Search' 
+//                 value={searchTxt}
+//                 onChange={(e) => {
+//                     setSearchTxt(e.target.value)
+//                 }}
+//                 />
+//                 <a href="#" onClick={() =>{
+
+//                 }} ><span className="material-symbols-outlined">search</span> </a>
 
                 
                 
-            </div>
-            <div>
-                <h5>{searchClick? "clicked":"unclicked"}</h5>
-            </div>
-        </>
+//             </div>
+//             <div>
+//                 <h5>{searchClick? "clicked":"unclicked"}</h5>
+//             </div>
+//         </>
 
 
-    )
+//     )
 }
 
 export default SearchBar;
