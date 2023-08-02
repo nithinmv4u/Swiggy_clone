875998db78fff9bd24005de027807d9a672d728d
nithinmv4u <nithinmv4u@gmail.com>
2023-08-02 16:13:27 +0530

    minor reconciliation example at search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,19 +3,39 @@ import { useState } from "react";
 const SearchBar = () => {
     // let searchTxt = "KFC"
     const [searchTxt, setSearchTxt] = useState("Indian Cuisines")
+
+    const [searchClick, setSearchClick] = useState(false)
+
     return (
-        <div className='search'>
-            <input 
-            className='search_img' 
-            type="text" 
-            placeholder='Search' 
-            value={searchTxt}
-            onChange={(e) => {
-                setSearchTxt(e.target.value)
-            }}
-            />
-            <span className="material-symbols-outlined">search</span>
-        </div>
+        <>
+            <div className='search'>
+                <input 
+                className='search_img' 
+                type="text" 
+                placeholder='Search' 
+                value={searchTxt}
+                onChange={(e) => {
+                    setSearchTxt(e.target.value)
+                }}
+                />
+                <a href="#" onClick={() =>{
+                    console.log("clicked " + searchClick);
+                    if (searchClick === false){
+                        setSearchClick(true);
+                    }  
+                    else {
+                        setSearchClick(false);
+                    }
+                }} ><span className="material-symbols-outlined">search</span> </a>
+
+                
+                
+            </div>
+            <div>
+                <h5>{searchClick? "clicked":"unclicked"}</h5>
+            </div>
+        </>
+
 
     )
 }
